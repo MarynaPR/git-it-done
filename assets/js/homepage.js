67f464ba1cbe56a88666af355afcb6ecaaf4e03b
MarynaPR <pryadkamaryna@gmail.com>
2020-10-06 15:52:42 -0500

    format api url, request to url

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,35 @@
 //     console.log("function was called");
 // };
 
-// getUserRepos(); update to=>
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+// // getUserRepos(); update to=>
+
+// // var getUserRepos = function () {
+// //     fetch("https://api.github.com/users/octocat/repos");
+// // };
+// //update to=>
+// var response = fetch("https://api.github.com/users/octocat/repos");
+// console.log(response);
+
+// fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+//     console.log("inside", response);
+// });
+
+// console.log("outside");
+
+fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    response.json().then(function (data) {
+        console.log(data);
+    });
+});
+
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
